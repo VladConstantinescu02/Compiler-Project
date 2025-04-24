459d85d879f73942e745a5ee1415f70f094f2226
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2025-04-24 16:44:06 +0300

    Structural changes to separate lexer from compiler

---
+++ Lexer.h
@@ -0,0 +1,33 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+// Token codes
+enum {
+    ID, CT_INT, CT_REAL, CT_CHAR, CT_STRING, COMMA, SEMICOLON, LPAR, RPAR, LBRACKET, RBRACKET,
+    LACC, RACC, ADD, SUB, MUL, DIV, DOT, AND, OR, EQUAL, ASSIGN, NOTEQ, NOT, LESSEQ, LESS,
+    GREATEREQ, GREATER, SPACE, BREAK, CHAR, END, DOUBLE, ELSE, FOR, IF, INT, RETURN, STRUCT,
+    VOID, WHILE
+};
+
+// Token structure
+typedef struct _Token {
+    int code;
+    union {
+        char *text;
+        long int i;
+        double r;
+    };
+    int line;
+    struct _Token *next;
+} Token;
+
+// Functions
+Token *addTk(int code);
+char *createString(const char *start, const char *end);
+int getNextToken();
+void showTokens();
+void done();
+char *readFileContent(const char *fileName);
+void setInput(const char *input);
+
+#endif // LEXER_H
